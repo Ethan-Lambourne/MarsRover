8a1a40aa8a8d1fbba72f1755508aa1fdee71e7fa
Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
2023-10-19 15:13:02 +0100

    Add project files.

---
+++ MarsRover/Program.cs
@@ -0,0 +1,103 @@
+﻿using MarsRover;
+
+Console.WriteLine(@"
+                =====================
+                WELCOME TO MARS ROVER
+                =====================
+
+Place the rover on the map and have a wander around!
+
+Use the WASD keys to move :)
+
+PRESS ENTER TO CONTINUE");
+Console.ReadLine();
+
+var _map = new Map();
+var _rover = new Rover(_map);
+
+_map.CreateNewMap();
+
+bool validationLoop = true;
+while (validationLoop)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            Console.Write(_map.marsMap[i, j]);
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine("\nWhat coordinates do you want to drop the rover at?");
+    Console.Write("X: ");
+    int xCoordinate = GetCoordinate(true);
+    Console.Write("Y: ");
+    int yCoordinate = GetCoordinate(false);
+    bool check = _rover.PlaceRover(xCoordinate, yCoordinate);
+    if (check)
+    {
+        validationLoop = false;
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Feel free to move your rover around the map! (press 'x' to exit)");
+
+bool playLoop = true;
+while (playLoop)
+{
+    Console.Clear();
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            Console.Write(_map.marsMap[i, j]);
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine($"Current Coordinates [{_rover.currentXcoordinate},{10 - _rover.currentYcoordinate}]");
+
+    switch (Console.ReadKey(true).Key)
+    {
+        case ConsoleKey.W:
+            _rover.MoveRover("north");
+            break;
+
+        case ConsoleKey.S:
+            _rover.MoveRover("south");
+            break;
+
+        case ConsoleKey.D:
+            _rover.MoveRover("east");
+            break;
+
+        case ConsoleKey.A:
+            _rover.MoveRover("west");
+            break;
+
+        case ConsoleKey.X:
+            playLoop = false;
+            break;
+    }
+}
+Console.WriteLine("\nThanks for playing! Goodbye!");
+
+int GetCoordinate(bool x)
+{
+    int number;
+    if (x)
+    {
+        while (!int.TryParse(Console.ReadLine(), out number) || number > 20 || number < 1)
+        {
+            Console.WriteLine("\nPlease enter a valid integer between 1-19.\n");
+        }
+    }
+    else
+    {
+        while (!int.TryParse(Console.ReadLine(), out number) || number > 10 || number < 1)
+        {
+            Console.WriteLine("\nPlease enter a valid integer between 1-9.\n");
+        }
+    }
+    return number;
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,115 @@
+﻿namespace MarsRover
+{
+    public class Rover
+    {
+        public int currentXcoordinate { get; set; }
+
+        public int currentYcoordinate { get; set; }
+
+        private readonly Map _map;
+
+        public Rover(Map map)
+        {
+            _map = map;
+        }
+
+        public void MoveRover(string direction)
+        {
+            var roverPosition = _map.marsMap.GetValue(currentYcoordinate, currentXcoordinate);
+            if (roverPosition as string == "x")
+            {
+                switch (direction)
+                {
+                    case "north":
+                        var newPosition = _map.marsMap.GetValue(currentYcoordinate - 1, currentXcoordinate);
+                        if (newPosition as string == " ")
+                        {
+                            UpdateRoverPosition("north");
+                        }
+                        break;
+
+                    case "south":
+                        newPosition = _map.marsMap.GetValue(currentYcoordinate + 1, currentXcoordinate);
+                        if (newPosition as string == " ")
+                        {
+                            UpdateRoverPosition("south");
+                        }
+                        break;
+
+                    case "east":
+                        newPosition = _map.marsMap.GetValue(currentYcoordinate, currentXcoordinate + 1);
+                        if (newPosition as string == " ")
+                        {
+                            UpdateRoverPosition("east");
+                        }
+                        break;
+
+                    case "west":
+                        newPosition = _map.marsMap.GetValue(currentYcoordinate, currentXcoordinate - 1);
+                        if (newPosition as string == " ")
+                        {
+                            UpdateRoverPosition("west");
+                        }
+                        break;
+                }
+            }
+        }
+
+        public void UpdateRoverPosition(string direction)
+        {
+            switch (direction)
+            {
+                case "north":
+                    _map.marsMap.SetValue(" ", currentYcoordinate, currentXcoordinate);
+                    _map.marsMap.SetValue("x", currentYcoordinate - 1, currentXcoordinate);
+                    currentYcoordinate--;
+                    break;
+
+                case "south":
+                    _map.marsMap.SetValue(" ", currentYcoordinate, currentXcoordinate);
+                    _map.marsMap.SetValue("x", currentYcoordinate + 1, currentXcoordinate);
+                    currentYcoordinate++;
+                    break;
+
+                case "east":
+                    _map.marsMap.SetValue(" ", currentYcoordinate, currentXcoordinate);
+                    _map.marsMap.SetValue("x", currentYcoordinate, currentXcoordinate + 1);
+                    currentXcoordinate++;
+                    break;
+
+                case "west":
+                    _map.marsMap.SetValue(" ", currentYcoordinate, currentXcoordinate);
+                    _map.marsMap.SetValue("x", currentYcoordinate, currentXcoordinate - 1);
+                    currentXcoordinate--;
+                    break;
+            }
+        }
+
+        public bool PlaceRover(int x, int y)
+        {
+            var check = _map.marsMap.GetValue(10 - y, x);
+            Console.WriteLine(check);
+
+            switch (check)
+            {
+                case "#":
+                    Console.WriteLine("Cannot drop the rover on an obsticle, choose another spot");
+                    return false;
+
+                case "|":
+                    Console.WriteLine("Cannot drop the rover on the edge of the map, choose another spot");
+                    return false;
+
+                case "_":
+                    Console.WriteLine("Cannot drop the rover on the edge of the map, choose another spot");
+                    return false;
+
+                default:
+                    _map.marsMap.SetValue("x", 10 - y, x);
+                    currentXcoordinate = x;
+                    currentYcoordinate = 10 - y;
+                    return true;
+            }
+        }
+    }
+}
